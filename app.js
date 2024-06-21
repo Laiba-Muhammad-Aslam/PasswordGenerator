a75aafd73e71bdeb7e0696620298b2846fdef438
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-20 18:15:33 -0700

    code for symbols

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ var lengthBox = document.getElementById("lengthbox");
 var lengthBar = document.getElementById("lengthbar");
 // console.log(lengthBox.innerText)
 console.log(lengthBar.value);
+
+var numbers = document.getElementById("numbers");
+var symbols = document.getElementById("symbols");
+console.log(numbers);
+console.log(symbols);
