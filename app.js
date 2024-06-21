297d713c3b977f1d756db38b25d22244319bcd11
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-21 15:00:42 -0700

    working on ui of password generator

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var lengthBox = document.getElementById("lengthbox");
 var lengthBar = document.getElementById("lengthbar");
+var passwordContainer = document.getElementById("passwordContainer");
 // console.log(lengthBox.innerText)
 console.log(lengthBar.value);
+lengthBox.innerText = lengthBar.value;
 
 var numbers = document.getElementById("numbers");
 var symbols = document.getElementById("symbols");
 console.log(numbers);
 console.log(symbols);
+
+
+var numbers = "0123456789";
+var symbols = "@.%#!";
+
+function generateRandomPassword(length){
+    var randomPassword = Math.floor(Math.random() * 10);
+    passwordContainer.innerText = randomPassword;
+}
+
+generateRandomPassword(lengthBar.value)
