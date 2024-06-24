4fc2377b2a0a87430948b309671c7f10c992201f
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-24 16:44:46 -0700

    functions for copy, update length and generate password

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-var lengthBox = document.getElementById("lengthbox");
-var lengthBar = document.getElementById("lengthbar");
 var passwordContainer = document.getElementById("passwordContainer");
-// console.log(lengthBox.innerText)
-console.log(lengthBar.value);
-lengthBox.innerText = lengthBar.value;
-
+var randomBtn = document.getElementById("randombtn");
+var refreshBtn = document.getElementById("refreshbtn");
+var copyBtn = document.getElementById("copybtn");
+var lengthBar = document.getElementById("lengthbar");
+var lengthBox = document.getElementById("lengthbox");
 var numbers = document.getElementById("numbers");
 var symbols = document.getElementById("symbols");
-console.log(numbers);
-console.log(symbols);
+var password = "";
 
+copyBtn.addEventListener("click", copy);
+randomBtn.addEventListener("click", generateRandomPassword);
+refreshBtn.addEventListener("click", generateRandomPassword);
+lengthBar.addEventListener("input", updateLength);
 
-var numbers = "0123456789";
-var symbols = "@.%#!";
+//Functions
 
-function generateRandomPassword(length){
-    var randomPassword = Math.floor(Math.random() * 10);
-    passwordContainer.innerText = randomPassword;
+function copy(){
+    navigator.clipboard.writeText(passwordContainer.value);
+    alert("Password copied to clipboard!");
 }
 
-generateRandomPassword(lengthBar.value)
+function updateLength(){
+    lengthBox.textContent = lengthBar.value;
+}
+updateLength();
+
+function generateRandomPassword(){
+    var length = parseInt(lengthBar.value);
+    var includeNumbers = numbers.checked;
+    var includeSymbols = symbols.checked;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    if(includeNumbers){
+        charset+= "0123456789";
+    }
+    if(includeSymbols){
+        charset += "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    }
+
+    password = "";
+    for(var i = 0; i < length; i++){
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    passwordContainer.value = password;
+
+}
+
